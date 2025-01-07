f003f2c57ddf39d35c06ac139390fb979ef18aa1
Dawid Markiewicz <146426182+goferwplynie@users.noreply.github.com>
2025-01-07 18:02:27 +0100

    Add files via upload

---
+++ weatherApp/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		Temp_c    float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Date string `json:"date"`
+			Day  struct {
+				Maxtemp_c float64 `json:"maxtemp_c"`
+				Mintemp_c float64 `json:"mintemp_c"`
+				Condition struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+			} `json:"day"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
+}
+
+func main() {
+	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=ca5ac2ea8e104811a31205754241012&q=Warsaw&days=7&aqi=no&alerts=no")
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		fmt.Println("Api not available")
+		return
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+
+	// Convert the weather object to JSON
+	weatherJSON, err := json.MarshalIndent(weather, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the JSON object
+	fmt.Println(string(weatherJSON))
+}
